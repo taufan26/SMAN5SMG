e6f61be3ab3c5dae806153ba4cab3ab1ce2f35b2
RizkyRockatansky <thenotoriousrizky@gmail.com>
2021-02-04 13:59:43 +0700

    oraoraora

--- app/src/main/java/app/klikgss/sman5smg/MainActivity.java
+++ app/src/main/java/app/klikgss/sman5smg/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.luseen.spacenavigation.SpaceItem;
+import com.luseen.spacenavigation.SpaceNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 }
