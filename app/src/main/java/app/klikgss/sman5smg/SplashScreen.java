f8fa3c577ee787831ca22f9b2e0dfbf6478574a0
RizkyRockatansky <thenotoriousrizky@gmail.com>
2021-02-04 09:44:13 +0700

    splash jadi

--- app/src/main/java/app/klikgss/sman5smg/SplashScreen.java
+++ app/src/main/java/app/klikgss/sman5smg/SplashScreen.java
@@ -2,13 +2,27 @@ package app.klikgss.sman5smg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
+    private int waktu_loading = 2000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(home);
+                finish();
+            }
+        }, waktu_loading);
+
     }
+
 }
