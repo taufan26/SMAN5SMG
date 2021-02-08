e615b4e61a6f0a57a8adecaee11da96fab10bfe5
RizkyRockatansky <thenotoriousrizky@gmail.com>
2021-02-08 13:52:37 +0700

    layoutMA

--- app/src/main/java/app/klikgss/sman5smg/MainActivity.java
+++ app/src/main/java/app/klikgss/sman5smg/MainActivity.java
@@ -3,15 +3,26 @@ package app.klikgss.sman5smg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+    TextView getnamasiswa, getkelas, getbingung;
+    ImageView getfotoprofil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        getfotoprofil= findViewById(R.id.fotoprofil);
+        getnamasiswa = findViewById(R.id.NamaSiswa);
+        getkelas = findViewById(R.id.Kelas);
+        getbingung = findViewById(R.id.bingung);
+
+
 
     }
 
